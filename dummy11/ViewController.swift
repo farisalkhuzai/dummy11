0936cfef39a50fc893f51236d9901bd67b160a0d
Faris Alkhuzai <alkhuzaifais@gmail.com>
2018-07-15 12:28:10 +0300

    first

---
+++ dummy11/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dummy11
+//
+//  Created by Faris Alkhuzai on 02/11/1439 AH.
+//  Copyright © 1439 Faris Alkhuzai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
